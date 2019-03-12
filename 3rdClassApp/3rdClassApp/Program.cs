f967866e319bfb290bd83ff2cbb236cd113904a6
Blake Jensen <30006830@student.toiohomai.ac.nz>
2019-03-13 10:19:14 +1300

    Program for W2 3rd app

---
+++ 3rdClassApp/3rdClassApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3rdClassApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            var num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            var num2 = int.Parse(Console.ReadLine());
+
+            int total = num1 + num2;
+            int total1 = num1 - num2;
+            int total2 = num1 * num2;
+            int total3 = num1 / num2;
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, total);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, total1);
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, total2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, total3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
